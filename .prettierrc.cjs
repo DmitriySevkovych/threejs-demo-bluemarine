7f8885849895208c62eb1b5fe7b90f788682a0ab
Dmitriy Sevkovych <d.sevkovych@varsandnumbers.com>
2023-10-16 06:07:36 +0200

    Initial commit

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+    trailingComma: "es5",
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+    importOrder: ["^@core/(.*)$", "^@server/(.*)$", "^@ui/(.*)$", "^[./]"],
+    importOrderSeparation: true,
+    importOrderSortSpecifiers: true,
+    plugins: ['@trivago/prettier-plugin-sort-imports']
+}
