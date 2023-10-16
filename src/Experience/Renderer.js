ff5bd2ac81c97a305c0df51c4e7ad8df38dfba3c
Dmitriy Sevkovych <d.sevkovych@varsandnumbers.com>
2023-10-16 11:11:57 +0200

    chore: add renderer debug ui for adjusting clearColor

--- src/Experience/Renderer.js
+++ src/Experience/Renderer.js
@@ -1,43 +1,54 @@
 import * as THREE from 'three'
+
 import Experience from './Experience.js'
 
-export default class Renderer
-{
-    constructor()
-    {
+export default class Renderer {
+    constructor() {
         this.experience = new Experience()
         this.canvas = this.experience.canvas
         this.sizes = this.experience.sizes
         this.scene = this.experience.scene
         this.camera = this.experience.camera
+        this.debug = this.experience.debug
+
+        if (this.debug.active) {
+            this.debugFolder = this.debug.ui.addFolder('Renderer')
+        }
 
         this.setInstance()
     }
 
-    setInstance()
-    {
+    setInstance() {
+        const parameters = {
+            clearColor: '#05233c',
+        }
         this.instance = new THREE.WebGLRenderer({
             canvas: this.canvas,
-            antialias: true
+            antialias: true,
         })
         this.instance.useLegacyLights = false
         this.instance.toneMapping = THREE.CineonToneMapping
         this.instance.toneMappingExposure = 1.75
         this.instance.shadowMap.enabled = true
         this.instance.shadowMap.type = THREE.PCFSoftShadowMap
-        this.instance.setClearColor('#211d20')
+        this.instance.setClearColor(parameters.clearColor)
+        this.instance.setSize(this.sizes.width, this.sizes.height)
+        this.instance.setPixelRatio(this.sizes.pixelRatio)
+
+        if (this.debug.active) {
+            this.debugFolder.addColor(parameters, 'clearColor').onChange(() => {
+                this.instance.setClearColor(parameters.clearColor)
+            })
+            this.debugFolder.open()
+        }
+    }
+
+    resize() {
         this.instance.setSize(this.sizes.width, this.sizes.height)
         this.instance.setPixelRatio(this.sizes.pixelRatio)
     }
 
-    resize()
-    {
-        this.instance.setSize(this.sizes.width, this.sizes.height)
-        this.instance.setPixelRatio(this.sizes.pixelRatio)
-    }
-
-    update()
-    {
+    update() {
         this.instance.render(this.scene, this.camera.instance)
     }
 }
